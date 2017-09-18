90629969c2cd1322d4545e87cf96b22e25edb83a
boodles <steven.garboden@gmail.com>
2017-09-17 21:53:54 -0700

    Update order_time.sql
    added subquery

--- order_time.sql
+++ order_time.sql
@@ -2,9 +2,14 @@ SELECT AVG(CASE
 			WHEN how_long_it_took_to_order_seconds = 0
 				THEN 469
 			ELSE how_long_it_took_to_order_seconds
-			END) / 60 ---
-FROM gar_bonzo.postmates_new;
-
+			END) / 60
+FROM (
+	SELECT delivery_id
+		,how_long_it_took_to_order_seconds
+	FROM gar_bonzo.postmates_new
+	GROUP BY 1
+		,2
+	) b
 
 SELECT avg(how_long_it_took_to_order_seconds) / 60 ---spot checking with zeros. included= 3.9min
 FROM gar_bonzo.postmates_new;
